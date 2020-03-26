6c525f6c0c17526195b1fb8f528d12f5d7c0ed29
Michael <mikemikhailov2003@gmail.com>
2020-03-26 18:00:47 +0200

    Deleted unused index.scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
-import './scss/index.scss';
 import 'antd/dist/antd.css';
 import App from './components/App';
 
